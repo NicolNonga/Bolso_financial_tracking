f28ac1c47ffdb9ba693ab9e24e2334d1ad60c76f
NicolNonga <nicodemosarmindo@gmail.com>
2025-09-18 14:50:30 +0100

    Initial Commit

---
+++ FinancialTracker/FinancialTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTrackerApp.swift
+//  FinancialTracker
+//
+//  Created by apple on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
